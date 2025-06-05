1350b0cb04dc8a487ec4dcb14a937d833cc1256e
Fernando Canales <fercanap@gmail.com>
2025-06-05 06:06:49 +0000

    Add loadbalancer module and integrate it in main.tf

--- labs/aws-final-lab/root/main.tf
+++ labs/aws-final-lab/root/main.tf
@@ -1,5 +1,5 @@
 module "network" {
-  source              = "./network"
+  source              = "../network"
   lab_name            = var.lab_name
   cidr_block          = var.cidr_block
   public_cidr_block_a = var.public_cidr_block_a
@@ -13,4 +13,31 @@ module "network" {
     Project = "aws-final-lab"
     Owner   = var.lab_name
   }
+}
+
+module "securityGroups" {
+  source  = "../securityGroups"
+  vpc_id  = module.network.vpc.id
+  tags    = {
+    Project = "aws-final-lab"
+    Owner   = var.lab_name
+  }
+}
+
+module "ec2" {
+  source           = "../ec2"
+  ami_id           = data.aws_ami.ubuntu.id
+  subnet_id        = module.network.public_subnet_a.id
+  ssh_key_name     = aws_key_pair.ec2_key.key_name
+  private_key_pem  = tls_private_key.privateKey.private_key_pem
+  sg_bastion_id    = module.securityGroups.sg_bastion.id
+  lab_name         = var.lab_name
+}
+
+module "loadbalancer" {
+  source            = "../loadbalancer"
+  vpc_id            = module.network.vpc.id
+  public_subnet_ids = [module.network.public_subnet_a.id, module.network.public_subnet_b.id]
+  sg_public_id      = module.securityGroups.sg_public.id
+  lab_name          = var.lab_name
 }
--- labs/aws-final-lab/root/output.tf
+++ labs/aws-final-lab/root/output.tf
@@ -7,3 +7,8 @@ output "public_subnet_a_id" {
   value       = module.network.public_subnet_a.id
   description = "ID de la subnet pública A"
 }
+
+output "bastion_ssh_command" {
+  description = "Comando SSH para conectarse al Bastion Host"
+  value       = "ssh -i key.pem ubuntu@${module.ec2.bastion_public_ip}"
+}
